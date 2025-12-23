95478e741c0ef7254e7eae879f82a4ff987bb9a5
shahad <sh1717161@gmail.com>
2025-12-23 17:16:43 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Shahad Alharbi on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
